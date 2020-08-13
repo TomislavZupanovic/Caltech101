c0e5071786165dc00fe5ad4e9b0c7a4176d99f39
TomislavZupanovic <tzupan01@fesb.hr>
2020-08-13 13:00:27 +0200

    Added Gradio interface option

--- predict.py
+++ predict.py
@@ -1,12 +1,25 @@
 from source.model import Model
-from source.preprocess import process_data
 from source.utils import random_image
+import gradio as gr
+import argparse
 
-train_data, valid_data, test_data, train_dataset = process_data('source/data/Dataset')
+parser = argparse.ArgumentParser()
+parser.add_argument('--usage', type=str, default='random',
+                    choices=['random', 'gradio'],
+                    help='Usage mode')
+args = parser.parse_args()
 model = Model()  # Instantiate model
 model.load_weights('model_Caltech101')
-model.predict(random_image())
 
+if args.usage == 'gradio':
+    def gr_predict(inp):
+        classes, top_prob = model.predict(inp)
+        return {classes[i]: float(top_prob[i]) for i in range(5)}
+    inputs = gr.inputs.Image(type='pil')
+    outputs = gr.outputs.Label(num_top_classes=4)
+    gr.Interface(fn=gr_predict, inputs=inputs, outputs=outputs).launch()
+else:
+    model.predict(random_image(), plot_pred=True)
 
 
 
